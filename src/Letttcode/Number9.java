3a4a323aa2ad59bbe5d5d5923092680669fc6350
akatsuke <1028572004@qq.com>
2019-08-06 10:06:52 +0800

    12
    2

---
+++ src/Letttcode/Number9.java
@@ -0,0 +1,26 @@
+package Letttcode;
+
+public class Number9 {
+    //判断一个整数是否是回文数。回文数是指正序（从左向右）和倒序（从右向左）读都是一样的整数。
+    //这道题是用于判断一个int数是否为回文数
+    public boolean isPalindrome(int x) {
+        //负数肯定不是回文数
+        if(x < 0){
+            return  false;
+        }
+        if (x == 0){
+            return  true;
+        }
+
+        String value = String.valueOf(x);
+        int length = value.length();
+        if (length == 1)return true;
+        int i = 0;
+        while (i <= length){
+            if (value.charAt(i++) != value.charAt(--length)){
+                return false;
+            }
+        }
+        return  true;
+    }
+}
